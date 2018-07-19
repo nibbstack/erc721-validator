e90349aa9b3c8baad3809581021904a7e6226531
Leon Panjtar <leon.panjtar@gmail.com>
2018-07-19 13:09:57 +0200

    Validator v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# erc721-validator
+<img src="assets/cover.png" />
+
+![Build Status](https://travis-ci.org/0xcert/erc721-validator.svg?branch=master)&nbsp;[![npm version](https://badge.fury.io/js/%400xcert%2Ferc721-validator.svg)](https://badge.fury.io/js/%400xcert%2Ferc721-validator)&nbsp;[![Dependency Status](https://gemnasium.com/0xcert/erc721-validator.svg)](https://gemnasium.com/0xcert/erc721-validator)
+
+> Ethereum ERC-721 Contract Validator.
+
+This is an open source package for NodeJS written with [TypeScript](https://www.typescriptlang.org). It allows for validating a contract against a series of tests to check its compliancy with the ERC-721 standard.
+
+This package is actively maintained, well tested and already used in production environments. The source code is available on [GitHub](https://github.com/0xcert/erc721-validator) where you can also find our [issue tracker](https://github.com/0xcert/erc721-validator/issues).
+
+## Installation
+
+Run the command below to install the package.
+
+```
+npm install --save web3 erc721-validator
+```
+
+This package uses promises thus you need to use [Promise polyfill](https://github.com/taylorhakes/promise-polyfill) when promises are not supported.
+
+## Getting started
+
+Initialize the Web3 provider.
+
+```js
+import * as Web3 from 'web3';
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+```
+
+Load and parse data of a particular block.
+
+```js
+import { Validator } from 'erc721-validator';
+
+const validator = new ERC721Validator(web3);
+const contract  = '0x...';
+const token     = '123';
+const giver     = '0x...';
+
+await validator.basic(test, contract);                  // => [{...},{...},{...},{...}]
+await validator.token(test, contract, token);           // => [{...},{...},{...},{...}]
+await validator.transfer(test, contract, token, giver); // => [{...},{...},{...},{...}]
+```
+
+## API
+
+### ERC721Validator Class
+
+**ERC721Validator(web3)**
+
+> Main class which allows for testing your contract validity.
+
+| Option | Type | Required | Default | Description
+|--------|------|----------|---------|------------
+| web3 | Web3 | Yes | - | Instance of a Web3 provider.
+
+**NOTICE:** The ERC721Validator class extends is a [RawModel class](https://github.com/xpepermint/rawmodeljs) and thus exposes all related helper methods.
+
+**ERC721Validator.prototype.basic(contract)**: Promise(JSON)
+
+> Performes a series of basic contract tests.
+
+| Option | Type | Required | Default | Description
+|--------|------|----------|---------|------------
+| contract | String | Yes | - | Contract Address
+
+**ERC721Validator.prototype.token(contract, tokenId)**: Promise(JSON)
+
+> Performes a series of tests to validate contract token compliancy.
+
+| Option | Type | Required | Default | Description
+|--------|------|----------|---------|------------
+| contract | String | Yes | - | Contract Address
+| tokenId | String | Yes | - | Token ID
+
+**ERC721Validator.prototype.transfer(contract, tokenId, giver)**: Promise(JSON)
+
+> Performes a series of tests to validate contract token transfer compliancy.
+
+| Option | Type | Required | Default | Description
+|--------|------|----------|---------|------------
+| contract | String | Yes | - | Contract Address
+| tokenId | String | Yes | - | Token ID
+| giver | String | Yes | - | Address of giver
+
+## License (MIT)
+
+Copyright (c) 2018 0xcert <admin@0xcert.org>.
---
+++ src/validator.ts
@@ -0,0 +1,128 @@
+import * as Web3 from 'web3';
+import codes from './config/codes';
+import { Model } from 'rawmodel';
+
+/**
+ * Ethereum contract validator.
+ */
+export class Validator extends Model {
+  protected web3: Web3;
+
+  /**
+   * Class constructor.
+   * @param {Web3} web3 Instance of Ethereum client.
+   */
+  public constructor(web3: Web3) {
+    super();
+    this.defineWeb3(web3);
+  }
+
+  /**
+   * Defines the `web3` field.
+   * @param ctx Request context.
+   */
+  protected defineWeb3(web3?: Web3) {
+    Object.defineProperty(this, 'web3', {
+      get: () => web3
+    });
+  }
+
+  /**
+   * Performs tests with the basic contract.
+   * @param options Test options.
+   */
+  public async basic(test: number, contract: string) {
+    return new Promise(async (resolve, reject) => {
+      if (!contract) {
+        reject('You must provide contract address as input');
+      }
+      try {
+        const validator = (this.web3.eth as any).Contract(codes.ABI_BASIC);
+        const results = [];
+
+        await validator
+          .deploy({
+            data: codes.DATA_BASIC,
+            arguments: [test, contract]
+          })
+          .estimateGas((err, gas) => {
+            if (!err) {
+              resolve(true);
+            } else if (String(err).includes('gas required exceeds allowance or always failing transaction')) {
+              resolve(false);
+            } else {
+              resolve(err);
+            }
+          });
+        } catch (e) {
+          reject(e);
+        }
+    });
+  }
+
+  /**
+   * Performs tests with the token contract.
+   * @param options Test options.
+   */
+  public async token(test: number, contract: string, tokenId: any) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!contract) { reject('You must provide contract address as input'); }
+        if (!tokenId) { reject('You must provide tokenId as input'); }
+
+        const validator = (this.web3.eth as any).Contract(codes.ABI_TOKEN);
+
+        await validator
+          .deploy({
+            data: codes.DATA_TOKEN,
+            arguments: [ test, contract, tokenId ]
+          })
+          .estimateGas((err, gas) => {
+            if (!err) {
+              resolve(true);
+            } else if (String(err).includes('gas required exceeds allowance or always failing transaction')) {
+              resolve(false);
+            } else {
+              resolve(err);
+            }
+          });
+        } catch (e) {
+          reject(e);
+        }
+    });
+  }
+
+
+  /**
+   * Performs tests with the token contract.
+   * @param options Test options.
+   */
+  public async transfer(test: number, contract: string, tokenId: any, giver: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!contract) { reject('You must provide contract address as input'); }
+        if (!tokenId) { reject('You must provide tokenId as input'); }
+        if (!giver) { reject('You must provide giver address as input'); }
+
+        const validator = (this.web3.eth as any).Contract(codes.ABI_TOKEN);
+
+        await validator
+          .deploy({
+            data: codes.DATA_TOKEN,
+            arguments: [ test, contract, tokenId, giver ]
+          })
+          .estimateGas((err, gas) => {
+            if (!err) {
+              resolve(true);
+            } else if (String(err).includes('gas required exceeds allowance or always failing transaction')) {
+              resolve(false);
+            } else {
+              resolve(err);
+            }
+          });
+        } catch (e) {
+          reject(e);
+        }
+    });
+  }
+}
